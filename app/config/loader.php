d3b4e8779d8e7b2a9fbf5bc074ace06ab9b726e5
Ioana-Roxana BUTNARIU <butnariuioana@yahoo.com>
2016-09-01 10:58:29 +0300

    adding composer,adding new config, new loader

--- app/config/loader.php
+++ app/config/loader.php
@@ -1,18 +1,27 @@
 <?php
 
-$loader = new \Phalcon\Loader();
+use Phalcon\Loader;
+
+$loader = new Loader();
 
 /**
  * We're a registering a set of directories taken from the configuration file
  */
-$loader->registerDirs([
-    APP_PATH . $config->application->controllersDir,
-    // APP_PATH . $config->application->pluginsDir,
-    APP_PATH . $config->application->libraryDir,
-    APP_PATH . $config->application->modelsDir,
-    APP_PATH . $config->application->formsDir
-])->register();
+$loader->registerNamespaces([
+    'Test\Controllers' => $config->application->controllersDir,
+    'Test\Models'      => $config->application->modelsDir,
+    'Test\Forms'       => $config->application->formsDir,
+    'Test\Library'             => $config->application->libraryDir
+]);
+
+
+
 
 $loader->registerClasses([
     'Services' => APP_PATH . 'app/Services.php'
 ]);
+$loader->register();
+
+
+// Use composer autoloader to load vendor classes
+require_once BASE_PATH . '/vendor/autoload.php';
